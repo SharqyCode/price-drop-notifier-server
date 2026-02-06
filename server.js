de319f79e2b991d278d5066b012c40dffc472505
sharqy <ebraheemelsharkawy2002@gmail.com>
2026-02-06 22:29:12 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/subscribe-price-drop', (req, res) => {
+    console.log(req.body);
+    const { widget_email } = req.body;
+    setTimeout(() => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (widget_email === "pewpew@gmail.com") {
+            return res.status(409).send({ ok: false, error: "already_subscribed" });
+        }
+        if (emailRegex.test(widget_email)) {
+            return res.status(200).send({ ok: true })
+        }
+        else {
+            return res.status(400).send({ ok: false, error: "invalid_email" })
+        }
+    }, 1000);
+
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+export default app;
